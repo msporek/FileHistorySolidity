9534b26f4e6d5c95a5a6c368f0f74f9197d437de
Michal Sporek <msporek@gmail.com>
2023-06-13 16:27:15 +0200

    Upgrade to use Solidity 0.8.20 as minimum.

--- contracts/FileHistoryLib.sol
+++ contracts/FileHistoryLib.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.13 < 0.9.0;
+pragma solidity >=0.8.20 < 0.9.0;
 
 /// @title Library with basic types for storing file history entries on the blockchain. 
 /// @author Michal Sporek
